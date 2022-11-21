9d8d1c4cc13964f47451aa204693642e9ef45177
prabahtvverma <prabhatvverma@gmail.com>
2022-11-21 11:22:50 +0530

    Leraning setTimeout and setInterval

--- javascript/asynchronous2.js
+++ javascript/asynchronous2.js
@@ -1,12 +1,30 @@
-const btn = document.querySelector(".btn");
+// const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", function (event) {
-    console.log("Button is Clicked....");
-});
+// btn.addEventListener("click", function (event) {
+//     console.log("Button is Clicked....");
+// });
 
-function test() {
-    for (let i = 0; i < 7000; i++) {
-        console.log("count : " + i);
-    }
+// function test() {
+//     for (let i = 0; i < 7000; i++) {
+//         console.log("count : " + i);
+//     }
+// }
+// test();
+
+function go() {
+    console.log("Going Somewhere ...");
 }
-test();
+// window.setTimeout(function(){
+let timer = window.setInterval(go, 2000);
+
+
+console.log("hello ..... normal excution....");
+console.log("hello ..... normal excution....");
+console.log("hello ..... normal excution....");
+console.log("hello ..... normal excution....");
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", function () {
+    window.clearInterval(timer);
+    console.log("Button Is Clicked ...");
+});
